181494a858e7d1698eae8cdf8d55449150e81728
Sellakumar <sellakumark@outlook.com>
2023-09-01 07:25:33 +0530

    ci: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import * as React from 'react';
-import { createRoot, type Root } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import Home from './components/Home/Home';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
 
-const root: Root = createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
